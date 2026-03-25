f37a949403db48f54319ff36eab5a918a9d2561b
kELLY-HENRY-TSANGA <tsangakelly06@gmail.com>
2026-03-25 16:36:54 +0100

    Ajout des étudiants J, A et V

--- bad.py
+++ bad.py
@@ -13,3 +13,11 @@ class SchoolClass:
 
     def add_student(self, student):
         self.students.append(student)
+
+        # Ajoute ceci tout à la fin de bad.py
+if __name__ == "__main__":
+    school_class = SchoolClass()
+    school_class.add_student(Student('J', 10, 12, 13))
+    school_class.add_student(Student('A', 8, 2, 17))
+    school_class.add_student(Student('V', 9, 14, 14))
+    print("Données de test ajoutées.")
